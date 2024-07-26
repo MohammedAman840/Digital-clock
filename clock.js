c48a666b9f2f4f461f305d14b2ac4011c1499e81
MohammedAman840 <amanmohammed840@gmail.com>
2024-07-27 01:47:18 +0530

    digital clock files

---
+++ clock.js
@@ -0,0 +1,17 @@
+let hrs=document.getElementById("hrs");
+let min=document.getElementById("min");
+let sec=document.getElementById("sec");
+let ampm=document.getElementById("ampm");
+setInterval(() => {
+   let date=new Date();
+   let h=date.getHours();
+   let m=date.getMinutes();
+   let s=date.getSeconds();
+   
+   if (h>12) {h=h-12;}
+   hrs.textContent=(h<10?"0":"")+h;
+   // min.textContent=(h<10?"0":"")+m;
+   min.textContent=m;
+   sec.textContent=(s<10?"0":"")+s;
+   ampm.textContent=h<=12?"AM":"PM";
+}, 1000);
